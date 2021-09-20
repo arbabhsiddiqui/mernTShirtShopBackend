db44a19caa14ad7ee7c6fa49b2064f48f086c1df
Arbab Hussain <zackh41@gmail.com>
2021-09-20 19:17:52 +0530

    fix code

--- controllers/product.js
+++ controllers/product.js
@@ -73,7 +73,7 @@ exports.getProduct = (req, res) => {
 exports.photo = (req, res, next) => {
   if (req.product.photo.data) {
     res.set("Content-Type", req.product.photo.contentType);
-    return res.send(req.product.photo);
+    return res.send(req.product.photo.data);
     next();
   }
 };
